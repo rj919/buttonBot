b007bad7d60cf44e4c4da34026bf79c8d86d6353
rcj1492 <richard@collectiveacuity.com>
2016-06-04 08:22:17 -0700

    adding initial html page templates;

--- server/app.py
+++ server/app.py
@@ -12,3 +12,18 @@ flask_args = {
 }
 app = Flask(**flask_args)
 
+@app.route('/')
+def dashboard_page():
+    return render_template('dashboard.html'), 200
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    config_args = {
+        'host': '0.0.0.0',
+        'port': 5000
+    }
+    app.run(**config_args)
+
